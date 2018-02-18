9c3ad2ea4e6c3c267daee2b2e458b36a6e5d0820
tcsx <username@users.noreply.github.com>
2018-02-17 22:34:24 -0800

    initialize environment

---
+++ student-information-system/src/main/java/com/tcsx/studentinfo/studentinformationsystem/StudentInformationSystemApplication.java
@@ -0,0 +1,12 @@
+package com.tcsx.studentinfo.studentinformationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentInformationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentInformationSystemApplication.class, args);
+	}
+}
---
+++ student-information-system/src/test/java/com/tcsx/studentinfo/studentinformationsystem/StudentInformationSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tcsx.studentinfo.studentinformationsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentInformationSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
